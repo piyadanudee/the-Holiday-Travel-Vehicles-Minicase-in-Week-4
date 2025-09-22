bea5354f7c8ae82649d5ba3b40bbfacfcf26fc9f
piyadanudee <hellopiiyada@gmail.com>
2025-09-22 18:58:48 +0700

    DealerOption.java

---
+++ DealerOption.java
@@ -0,0 +1,10 @@
+public class DealerOption {
+    private String optionCode;
+    private String description;
+    private double price;
+
+    public DealerOption(String optionCode, String description, double price) {
+        this.optionCode = optionCode;
+        this.description = description;
+        this.price = price;
+    }
