f3bf24b61daf615b1e2fad6bf14e2137e2d96f68
piyadanudee <hellopiiyada@gmail.com>
2025-09-22 18:25:43 +0700

    Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,12 @@
+import java.util.List;
 
+public class Customer {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public Customer(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
