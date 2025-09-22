a7b611af6cf6fa06502fa84b141c7747ade4a81f
piyadanudee <hellopiiyada@gmail.com>
2025-09-22 19:02:13 +0700

    SalePerson.java

--- SalePerson.java
+++ SalePerson.java
@@ -6,3 +6,27 @@ public class SalesPerson {
         this.name = name;
         this.saleID = saleID;
     }
+        public Invoice createInvoice(
+            Customer customer,
+            Vehicle vehicle,
+            double negotiatedPrice,
+            double taxes,
+            double licenseFees,
+            List<DealerOption> options,
+            TradeInVehicle tradeIn
+    ) {
+        Invoice inv = new Invoice(
+                Ids.nextInvoiceId(),
+                customer.getName(),
+                vehicle,
+                negotiatedPrice,
+                taxes,
+                licenseFees,
+                tradeIn
+        );
+        if (options != null) {
+            for (DealerOption opt : options) inv.addOptionToInvoice(opt);
+        }
+        return inv;
+    }
+
