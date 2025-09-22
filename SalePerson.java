59c9d2b7f72a996791d0a3da1a3a567e5c1e496a
piyadanudee <hellopiiyada@gmail.com>
2025-09-22 18:56:50 +0700

    SalePerson.java

---
+++ SalePerson.java
@@ -0,0 +1,8 @@
+public class SalesPerson {
+    private String name;
+    private int saleID;
+
+    public SalesPerson(String name, int saleID) {
+        this.name = name;
+        this.saleID = saleID;
+    }
