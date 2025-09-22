c7fd766d61d3e27bd2cd17603922b34560c41087
piyadanudee <hellopiiyada@gmail.com>
2025-09-22 18:57:36 +0700

    Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,16 @@
+public class Vehicle {
+    private String serialNumber;
+    private String name;
+    private String model;
+    private int year;
+    private String manufacturer;
+    private double baseCost;
+
+    public Vehicle(String serialNumber, String name, String model, int year, String manufacturer, double baseCost) {
+        this.serialNumber = serialNumber;
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.manufacturer = manufacturer;
+        this.baseCost = baseCost;
+    }
